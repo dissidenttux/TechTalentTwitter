30b2742d76bc64c456421ce24c8677e955517221
Michael "Cal" Krug <cal.krug@proton.me>
2022-06-28 14:55:16 -0500

    Add: initial commit

---
+++ src/main/java/com/tts/TechTalentTwitter/TechTalentTwitterApplication.java
@@ -0,0 +1,15 @@
+package com.tts.TechTalentTwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTalentTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTalentTwitterApplication.class, args);
+
+		System.out.println("Hello World");
+	}
+
+}
